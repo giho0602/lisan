495b50222d5c764ab20f49b2a99e451571394ff0
giho0602 <88920506+giho0602@users.noreply.github.com>
2021-08-27 16:12:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-    freeMode: true,
+    breakpoints: { //반응형 조건 속성
+      320: { //640 이상일 경우
+        slidesPerView: 1, //레이아웃 2열
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
 
 // section04 tab
 
